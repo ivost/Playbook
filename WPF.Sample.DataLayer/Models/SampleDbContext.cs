1b9c43ce8d7dd8a94d84528b944ed266682c6b20
Ivo Stoyanov <ivost100@gmail.com>
2020-01-30 13:55:32 -0800

    initial

---
+++ WPF.Sample.DataLayer/Models/SampleDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+
+namespace WPF.Sample.DataLayer
+{
+  public partial class SampleDbContext : DbContext
+  {
+    public SampleDbContext() : base("name=Samples")
+    {
+            Debug.WriteLine($"SampleDbContext()");
+                
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+  }
+}
