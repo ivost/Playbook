12340b8493069b9797202d06a34fb426575271eb
Ivo Stoyanov <ivost100@gmail.com>
2020-01-30 16:55:55 -0800

    splash screen begins

--- WPF.Sample.ViewModelLayer/MainWindowViewModel.cs
+++ WPF.Sample.ViewModelLayer/MainWindowViewModel.cs
@@ -2,31 +2,64 @@
 
 namespace WPF.Sample.ViewModelLayer
 {
-  public class MainWindowViewModel : ViewModelBase
-  {
-    #region Private Variables
-    private string _LoginMenuHeader = "Login";
-    private string _StatusMessage;
-    #endregion
-
-    #region Public Properties
-    public string LoginMenuHeader
+    public class MainWindowViewModel : ViewModelBase
     {
-      get { return _LoginMenuHeader; }
-      set {
-        _LoginMenuHeader = value;
-        RaisePropertyChanged("LoginMenuHeader");
-      }
-    }
+        private string _LoginMenuHeader = "Login";
+        private string _StatusMessage;
 
-    public string StatusMessage
-    {
-      get { return _StatusMessage; }
-      set {
-        _StatusMessage = value;
-        RaisePropertyChanged("StatusMessage");
-      }
+        private bool _IsInfoMessageVisible = true;
+        public bool IsInfoMessageVisible
+        {
+            get { return _IsInfoMessageVisible; }
+            set
+            {
+                _IsInfoMessageVisible = value;
+                RaisePropertyChanged("IsInfoMessageVisible");
+            }
+        }
+
+        private string _InfoMessage;
+        public string InfoMessage
+        {
+            get { return _InfoMessage; }
+            set
+            {
+                _InfoMessage = value;
+                RaisePropertyChanged("InfoMessage");
+            }
+        }
+
+        private string _InfoMessageTitle;
+        public string InfoMessageTitle
+        {
+            get { return _InfoMessageTitle; }
+            set
+            {
+                _InfoMessageTitle = value;
+                RaisePropertyChanged("InfoMessageTitle");
+            }
+        }
+
+        public string LoginMenuHeader
+        {
+            get { return _LoginMenuHeader; }
+            set
+            {
+                _LoginMenuHeader = value;
+                RaisePropertyChanged("LoginMenuHeader");
+            }
+        }
+
+        public string StatusMessage
+        {
+            get { return _StatusMessage; }
+            set
+            {
+                _StatusMessage = value;
+                RaisePropertyChanged("StatusMessage");
+            }
+        }
+
+
     }
-    #endregion
-  }
 }
